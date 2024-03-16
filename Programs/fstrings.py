dc89b24baba769f0346256559143eb02ce250d63
kl-priyanka <priyankz432@gmail.com>
2024-03-16 11:23:27 +0530

    16-03-2024

---
+++ Programs/fstrings.py
@@ -0,0 +1,7 @@
+#formatted strings
+first = 'Jungkook'
+last = 'Jeon'
+message = first + ' ['+last+'] is a singer'
+print (message)
+msg = f'{first} [{last}] is a singer'
+print(msg)
---
+++ Programs/strmet.py
@@ -0,0 +1,19 @@
+#string methods
+msg= 'Priyanka'
+print(len(msg))
+print(msg.upper())
+print(msg.lower())
+print(msg.find('a'))
+print(msg.find('z'))
+print(msg)
+m='Hi There'
+print(m.find('There'))
+print(m.replace('There', "Y'all"))
+print(m.replace('there', "Y'all"))
+print(m)
+print(m.replace(' ', '_'))
+print('------------------------------------')
+print('Priyanka' in msg)
+print('priyanka' in msg)
+p='bruh'
+print(p.title())
